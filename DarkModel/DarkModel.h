7dc9b926d586a6b5da01e5e80eaea1d10e981ae8
Dark Dong <viva@163.com>
2017-08-05 11:35:57 +0800

    Initial Commit

---
+++ DarkModel/DarkModel.h
@@ -0,0 +1,19 @@
+//
+//  DarkModel.h
+//  DarkModel
+//
+//  Created by Dark Dong on 2017/8/5.
+//  Copyright © 2017年 Dark Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DarkModel.
+FOUNDATION_EXPORT double DarkModelVersionNumber;
+
+//! Project version string for DarkModel.
+FOUNDATION_EXPORT const unsigned char DarkModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DarkModel/PublicHeader.h>
+
+
